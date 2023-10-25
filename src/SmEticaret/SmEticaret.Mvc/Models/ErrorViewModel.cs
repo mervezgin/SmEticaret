cb2b11953558bbc5b602ef9223ac75d42c20324f
Merve <mervezgin@gmail.com>
2023-10-25 16:30:05 +0300

    initial commit

---
+++ src/SmEticaret/SmEticaret.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmEticaret.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
