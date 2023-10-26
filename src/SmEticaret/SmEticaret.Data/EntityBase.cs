1874b80c6bf85fd9055856ee1f1f20cd877bcc2d
Merve <mervezgin@gmail.com>
2023-10-26 13:12:19 +0300

    Update EntityBase

--- src/SmEticaret/SmEticaret.Data/EntityBase.cs
+++ src/SmEticaret/SmEticaret.Data/EntityBase.cs
@@ -1,7 +1,13 @@
-﻿namespace SmEticaret.Data
+﻿using System.CodeDom.Compiler;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmEticaret.Data
 {
-    public class EntityBase
+    public abstract class EntityBase
     {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
     }
 }
