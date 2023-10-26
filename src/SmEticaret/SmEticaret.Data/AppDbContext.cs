2cb3ff31aa416180b6a52fcce686003798b19de4
Merve <mervezgin@gmail.com>
2023-10-26 13:18:44 +0300

    Update AppDbContext.cs

--- src/SmEticaret/SmEticaret.Data/AppDbContext.cs
+++ src/SmEticaret/SmEticaret.Data/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SmEticaret.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,22 @@ using System.Threading.Tasks;
 
 namespace SmEticaret.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<CartEntity> Carts { get; set; }
+        public DbSet<CartItemEntity> CartItems { get; set; }
+        public DbSet<CategoryEntity> Categories { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
+        public DbSet<OrderItemEntity> OrderItems { get; set; }
+        public DbSet<ProductCommentEntity> ProductComments { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<RoleEntity> Roles { get; set; }
+        public DbSet<UserEntity> Users { get; set; }
+
     }
 }
