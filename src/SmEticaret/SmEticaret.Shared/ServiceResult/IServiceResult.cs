d3a7b79f288bb1c2a32a39412acdb80e29dcf567
Merve <mervezgin@gmail.com>
2023-10-28 16:01:35 +0300

    Added Service Result

--- src/SmEticaret/SmEticaret.Shared/ServiceResult/IServiceResult.cs
+++ src/SmEticaret/SmEticaret.Shared/ServiceResult/IServiceResult.cs
@@ -8,5 +8,13 @@ namespace SmEticaret.Shared.ServiceResult
 {
     public interface IServiceResult
     {
+        bool IsSuccess { get; }
+        string? Message { get; }
+        int StatusCode { get; }
     }
+    public interface IServiceResult<out T> : IServiceResult
+    {
+        T? Data { get; }
+    }
+    
 }
