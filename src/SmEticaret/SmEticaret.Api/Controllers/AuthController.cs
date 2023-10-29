c12ebee1149c9160bf2daf57538405df6acce369
Merve <mervezgin@gmail.com>
2023-10-29 19:10:19 +0300

    Added Token Service

--- src/SmEticaret/SmEticaret.Api/Controllers/AuthController.cs
+++ src/SmEticaret/SmEticaret.Api/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using SmEticaret.Api.Models;
+using SmEticaret.Api.Services.TokenService;
 using SmEticaret.Data;
 using SmEticaret.Data.Entities;
 using System.IdentityModel.Tokens.Jwt;
@@ -19,11 +20,13 @@ namespace SmEticaret.Api.Controllers
     {
         private readonly AppDbContext _dbContext;
         private readonly IConfiguration _configuration;
+        private readonly ITokenService _tokenService;
 
-        public AuthController(AppDbContext dbContext, IConfiguration configuration)
+        public AuthController(AppDbContext dbContext, IConfiguration configuration, ITokenService tokenService)
         {
             _dbContext = dbContext;
             _configuration = configuration;
+            _tokenService = tokenService;
         }
 
         [HttpPost("login")]
@@ -50,55 +53,17 @@ namespace SmEticaret.Api.Controllers
                 return NotFound(); 
             }
 
-            var token = GetJwt(user);
+            var tokenResult = _tokenService.CreateToken(user);
+
+            if(!tokenResult.IsSuccess) 
+            {
+                return StatusCode(tokenResult.StatusCode, tokenResult.Message);
+            }
             return Ok(new
             {
-                Token = token
+                Token = tokenResult.Data
             });
         }
-
-        private string GetJwt(UserEntity user)
-        {
-            var claims = new List<Claim>  
-            {
-                new Claim(JwtClaimTypes.Name, user.Name),
-                new Claim(JwtClaimTypes.FamilyName, user.LastName),
-                new Claim(JwtClaimTypes.Email, user.Email),
-                new Claim(JwtClaimTypes.Role, user.Role.Name ),
-            };
-
-            string secret = GetSecretKeyFromConfiguration();
-            string issuer = GetIssuerFromConfiguration();
-            string audience = GetAudienceFromConfiguration();
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-            var jwtSecurityToken = new JwtSecurityToken(
-                issuer: issuer,
-                audience: audience,
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(30),
-                signingCredentials: credentials);
-            return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
-            
-        }
-
-        private string GetAudienceFromConfiguration()
-        {
-            return _configuration["Jwt:Audience"];
-        }
-
-        private string GetIssuerFromConfiguration()
-        {
-            return _configuration["Jwt:Issuer"];
-        }
-
-        private string GetSecretKeyFromConfiguration()
-        {
-            return _configuration["Jwt:Secret"];
-        }
-
         private string HashString(string input)
         {
             using var sha256 = SHA256.Create();
